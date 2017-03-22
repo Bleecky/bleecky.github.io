1cea3e07b71745cb2a64d59d76dab628399c40f0
Peter Bleeck <peter.bleeck@os.uk>
2017-03-22 12:52:57 +0000

    Initial commit

---
+++ Docs/compute-window.md
@@ -0,0 +1,20 @@
+# Information on using <template_name>
+
+## Template description
+
+### Parameters
+  Example Parameter 1
+
+  Example Parameter 2
+
+  Example Parameter 3
+
+### Using this template
+
+#### Using the template as a linked template
+
+    This is an example of some code for the template
+
+#### Key Information if using the template as an example.
+
+    Key Usage Points
---
+++ Utils/generate-docs.ps1
@@ -0,0 +1,26 @@
+﻿cd $PSScriptRoot
+$templates = Get-ChildItem "..\Templates\" -Filter *.json
+
+ForEach ($template in $templates){
+  $inFile = $template.Name
+  $fileName = $inFile.TrimEnd('.json')
+  $outFile = $fileName + ".md"
+  $fileExists = Test-Path "..\Docs\$outfile"
+  if ($fileExists){
+      "Markdown file $outFile already exists"
+    } else {
+      New-Item -Path "..\Docs" -Name $outFile -ItemType "file" -Value "This is the initial markdown for $infile"
+      $content = Get-Content -Path "template.md"
+      Set-Content -Path "..\Docs\$outFile" -Value $content 
+    }
+  
+  $templateParams = Get-Content -Path "..\Templates\$inFile" | ConvertFrom-Json
+  foreach($param in ($templateParams.parameters | Get-Member -MemberType NoteProperty).Name){
+    $paramType = ($templateParams.parameters.$param).type
+    $paramValue = ($templateParams.parameters.$param).defaultValue
+    
+    Add-Content -Path "..\Docs\params.md" -Value "#### Parameter Name : $param"
+    Add-Content -Path "..\Docs\params.md" -Value "    Type: $paramType"
+    Add-Content -Path "..\Docs\params.md" -Value "    Default Value: $paramValue"
+  }
+  }
